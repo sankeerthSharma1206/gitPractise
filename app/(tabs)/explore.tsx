4178b42ba18a3f3f7e9827a08b2ac6da876958ad
sankeerthSharma1206 <saisankeerthb@gmail.com>
2024-08-07 11:44:26 -0500

    changes

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -27,7 +27,7 @@ export default function TabTwoScreen() {
           sets up the tab navigator.
         </ThemedText>
         <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
+          <ThemedText type="link">sankeerth</ThemedText>
         </ExternalLink>
       </Collapsible>
       <Collapsible title="Android, iOS, and web support">
